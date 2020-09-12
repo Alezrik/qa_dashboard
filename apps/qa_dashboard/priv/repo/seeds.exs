6da95bea43a2d134f6f326af6db2d6b6f4c8e1c7
Alezrik <ex_ra@hotmail.com>
2020-09-12 05:08:04 -0700

    add user and role seeds

--- apps/qa_dashboard/priv/repo/seeds.exs
+++ apps/qa_dashboard/priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+{:ok, super_admin_role} = QaDashboard.Permissions.create_role(%{name: "super_admin"})
+{:ok, admin_role}= QaDashboard.Permissions.create_role(%{name: "admin"})
+{:ok, user_role} = QaDashboard.Permissions.create_role(%{name: "user"})
+
+
+super_admin_user = QaDashboard.Accounts.register_user(%{email: "superadmin@admin.com", password: "abcdef123456", role_id: super_admin_role.id})
+admin_user = QaDashboard.Accounts.register_user(%{email: "admin@admin.com", password: "abcdef123456", role_id: admin_role.id})
+user = QaDashboard.Accounts.register_user(%{email: "user@admin.com", password: "abcdef123456", role_id: user_role.id})
